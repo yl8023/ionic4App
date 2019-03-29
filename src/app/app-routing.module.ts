516ef529c8c5f82b731db2b844d4479a8d32667a
yl <820827816@qq.com>
2019-03-29 08:57:27 +0800

    updata

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'login', loadChildren: './page/login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './page/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './page/register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { LocalstorageService } from './localstorage.service';
+import { HttpClient } from '@angular/common/http';
 // import { ConfigModel } from '../models/config';
 
 @Injectable({
@@ -7,11 +8,14 @@ import { LocalstorageService } from './localstorage.service';
 })
 export class ConfigService {
     login_status: boolean;
-  constructor(public localSer: LocalstorageService) { }
-
+  constructor(public localSer: LocalstorageService,
+    public http: HttpClient) { }
   validationStatus() {
     let login = this.localSer.getLocal('login_status') || false;
     this.login_status = login;
     return this.login_status;
+  }
+  getData() {
+    // this.http.get()
   }
 }
